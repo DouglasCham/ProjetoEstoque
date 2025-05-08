c15123a8025097ba9147a91bc7d58fd1aac69d67
DouglasCham <douglaschampinotti@gmail.com>
2025-05-08 20:21:43 -0300

    Create Project Files

---
+++ ProjetoEstoque/src/test/java/com/api/ProjetoEstoque/ProjetoEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ProjetoEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
